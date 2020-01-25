fce759f5987ac467c1cf88819fcbc3439e81c658
Hp <Hp@DESKTOP-RLP0D7K>
2020-01-25 12:38:06 +0530

    first commit all codes

---
+++ hello/src/hello/com/hellodemo.java
@@ -0,0 +1,14 @@
+package hello.com;
+
+import hello.comr.user;
+
+public class hellodemo {
+
+	public static void main(String[] args)
+	{
+	 System.out.println("hi im java!!!!");
+	 user o = new user();
+	 o.add();
+	}
+
+}
---
+++ hello/src/hello/comr/user.java
@@ -0,0 +1,22 @@
+package hello.comr;
+
+public class user 
+{
+    int i=10;
+    int u=20;
+     
+    public void add()
+    {
+	   System.out.println("I+U= " +(i+u));	
+	}
+    private void multi()
+    {
+	System.out.println("I*U="+(i*u));
+
+	}
+  
+}
+
+
+
+
